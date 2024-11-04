a125f8e2dc72c89b8108d6b37d6f25ca2121c82d
Jacob <jacobguerrant01@gmail.com>
2024-11-04 17:34:40 -0500

    Fixed SampleCodeApplication.java main method.

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -10,24 +10,25 @@ public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
+
+		DisplayMessage displayMessage = new DisplayMessage();
+
+		// Create US thread
+		Thread threadUS = new Thread(() -> {
+			Locale locale = Locale.US;
+			String message = displayMessage.getWelcomeMessage(locale);
+			System.out.println("US Thread: " + message);
+		});
+
+		// Create FR thread
+		Thread threadFR = new Thread(() -> {
+			Locale locale = Locale.CANADA_FRENCH;
+			String message = displayMessage.getWelcomeMessage(locale);
+			System.out.println("FR Thread: " + message);
+		});
+
+		threadUS.start();
+		threadFR.start();
 	}
 
-	DisplayMessage displayMessage = new DisplayMessage();
-
-	// Create US thread
-	Thread threadUS = new Thread(() -> {
-		Locale locale = Locale.US;
-		String message = displayMessage.getWelcomeMessage(locale);
-		System.out.println("US Thread: " + message);
-	});
-
-	// Create FR thread
-	Thread threadFR = new Thread(() -> {
-		Locale locale = Locale.CANADA_FRENCH;
-		String message = displayMessage.getWelcomeMessage(locale);
-		System.out.println("FR Thread: " + message);
-	});
-
-	threadUS.start();
-	threadFR.start();
 }
